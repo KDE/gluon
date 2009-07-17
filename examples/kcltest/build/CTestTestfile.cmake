b1a66017eb7af7765157f92c5fb959a3f6c341f2
dridk <istdasklar@gmail.com>
2009-07-17 22:06:55 +0200

    remove unnecessary file

--- examples/kcltest/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sacha/programmation/gluon/examples/kcltest
-# Build directory: /home/sacha/programmation/gluon/examples/kcltest/build
-# 
-# This file replicates the SUBDIRS() and ADD_TEST() commands from the source
-# tree CMakeLists.txt file, skipping any SUBDIRS() or ADD_TEST() commands
-# that are excluded by CMake control structures, i.e. IF() commands.
