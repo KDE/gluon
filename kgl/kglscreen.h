f34678e97653b8edef795103f64c3edeaf2de174
Albert Astals Cid <aacid@kde.org>
2009-10-11 11:46:50 +0200

    remove template declaration of QPair that probably got there by mistake

--- kgl/kglscreen.h
+++ kgl/kglscreen.h
@@ -1,6 +1,4 @@
-
-template<class T1, class T2 >
-struct QPair;/*
+/*
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License version 2 as published by the Free Software Foundation.
