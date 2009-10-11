5d7237f2b37c9bd39bb0add29ea29dc80f2a1752
Albert Astals Cid <aacid@kde.org>
2009-10-11 14:56:01 +0200

    Add i18n

--- kcl/widgets/kclinfowidget.cpp
+++ kcl/widgets/kclinfowidget.cpp
@@ -28,8 +28,8 @@ KCLInfoWidget::KCLInfoWidget(QWidget *parent)
     m_information = new QWidget;
     m_preview = new QWidget;
     KTabWidget *tab = new KTabWidget;
-    tab->addTab(m_preview, KIcon("run-build-configure.png"), "preview");
-    tab->addTab(m_information, KIcon("run-build-file.png"), "information");
+    tab->addTab(m_preview, KIcon("run-build-configure.png"), i18n("Preview"));
+    tab->addTab(m_information, KIcon("run-build-file.png"), i18n("Information"));
     setupPreview();
     setupInformation();
     layout->addWidget(tab);
@@ -64,8 +64,8 @@ void KCLInfoWidget::setupInformation()
     QLabel *info = new QLabel(i18n("Some Linux distributions forbid direct access \n to the input method used by KCL(evdev) as it can causes security-related issues\n"
                                     "If you are sure, you can allow direct access by clicking here."));
 
-    KPushButton *buttonAuthorizate = new KPushButton(KIcon("dialog-password.png"), "set permission");
-    KPushButton *buttonDetect = new KPushButton(KIcon("edit-find.png"), "Detect device");
+    KPushButton *buttonAuthorizate = new KPushButton(KIcon("dialog-password.png"), i18n("Set permissions"));
+    KPushButton *buttonDetect = new KPushButton(KIcon("edit-find.png"), i18n("Detect device"));
 
     connect(buttonAuthorizate, SIGNAL(clicked()), this, SLOT(setAuthorization()));
     connect(buttonDetect, SIGNAL(clicked()), this, SLOT(detectDevice()));
@@ -89,9 +89,9 @@ void KCLInfoWidget::setAuthorization()
 {
     //TODO: Find a better way to do that(either via HAL, PolicyKit, or use X11 libs instead of evdev.
     if (QProcess::execute("kdesudo chmod -R a+rwx /dev/input/*")) {
-        KMessageBox::information(this, i18n("authorization success..."), "success");
+        KMessageBox::information(this, i18n("authorization success..."), i18n("success"));
     } else {
-        KMessageBox::error(this, i18n("authorization failed..."), "failed");
+        KMessageBox::error(this, i18n("authorization failed..."), i18n("failed"));
     }
 }
 
