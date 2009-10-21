c18d8663c41ed743ac798274c3f3fa7c9228b873
Dan Leinir Turthra Jensen <admin@leinir.dk>
2009-10-21 13:19:20 +0200

    Rename gdlexample to gluoncreator (plus window title and such)

--- creator/main.cpp
+++ creator/main.cpp
@@ -7,9 +7,9 @@
 #include "mainwindow.h"
 
 int main(int argc, char **argv) {
-    KAboutData aboutData("gdlexample", NULL,
-                        ki18n("GDL Example"), "1.0",
-                        ki18n("GDL Example"),
+    KAboutData aboutData("gluoncreator", NULL,
+                        ki18n("Gluon Creator"), "1.0",
+                        ki18n("Gluon Creator"),
                         KAboutData::License_GPL,
                         ki18n("Copyright (c) 2009 Developer"));
                         KCmdLineArgs::init(argc, argv, &aboutData);
