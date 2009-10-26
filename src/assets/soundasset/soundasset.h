477e6b41ed4ecbff202dc94a4fe816f06e7bd818
Guillaume Martres <smarter@ubuntu.com>
2009-10-26 21:52:21 +0100

    Added work-in-progress SoundAsset and SoundEmitterComponent

---
+++ src/assets/soundasset/soundasset.h
@@ -0,0 +1,41 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef SOUNDASSET_H
+#define SOUNDASSET_H
+
+#include "asset.h"
+#include "kal/kalbuffer.h"
+
+class SoundAsset : public Gluon::Asset
+{
+public:
+    SoundAsset(QObject *parent = 0);
+
+    virtual Gluon::GluonObject *instantiate();
+
+    KALBuffer *buffer() {
+        return m_buffer;
+    }
+
+private slots:
+    void setBuffer();
+
+private:
+    KALBuffer *m_buffer;
+};
+
+#endif // SOUNDASSET_H
---
+++ src/components/soundcomponents/soundemittercomponent.cpp
@@ -0,0 +1,44 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "soundemittercomponent.h"
+
+#include <QtCore/QDebug>
+
+using namespace Gluon;
+
+REGISTER_OBJECTTYPE(SoundEmitterComponent)
+
+SoundEmitterComponent::SoundEmitterComponent(QObject *parent)
+    : Component(parent)
+{
+}
+
+SoundEmitterComponent::SoundEmitterComponent(const Gluon::SoundEmitterComponent &other)
+    : Component(other)
+{
+}
+
+GluonObject *SoundEmitterComponent::instantiate()
+{
+    return new SoundEmitterComponent(this);
+}
+
+void SoundEmitterComponent::Update(int elapsedMilliseconds)
+{
+    qDebug() << "Hello, World";
+}
+
