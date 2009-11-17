d11c7db0ea9e25f17b19b25a39fe12af6c0fad13
Arjen-Wander Hiemstra <ahiemstra@ahiemstra-home.sshunet.nl>
2009-11-17 17:24:50 +0100

    Add Components dock plugin

---
+++ creator/plugins/docks/componentsdock/componentsdock.cpp
@@ -0,0 +1,70 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "componentsdock.h"
+
+#include <QListWidget>
+#include <gluonobjectfactory.h>
+#include <gluonobject.h>
+#include <component.h>
+#include <KDebug>
+
+using namespace Gluon::Creator;
+
+class ComponentsDock::ComponentsDockPrivate
+{
+    public:
+        ComponentsDockPrivate() { view = 0; };
+
+        QListWidget *view;
+};
+
+ComponentsDock::ComponentsDock(const QString& title, QWidget* parent, Qt::WindowFlags flags): Dock(title, parent, flags)
+{
+    setObjectName("ComponentsDock");
+
+    d = new ComponentsDockPrivate();
+    d->view = new QListWidget(this);
+
+    QHash<QString, Gluon::GluonObject*> objectTypes = Gluon::GluonObjectFactory::instance()->objectTypes();
+    foreach(Gluon::GluonObject* obj, objectTypes)
+    {
+        if(obj->metaObject()->superClass()->className() == QString("Gluon::Component")) d->view->addItem(obj->metaObject()->className());
+    }
+
+    setWidget(d->view);
+}
+
+ComponentsDock::~ComponentsDock()
+{
+    delete d;
+}
+
+void ComponentsDock::setSelection(Gluon::GluonObject* obj)
+{
+    Q_UNUSED(obj);
+}
+
+QAbstractItemView* ComponentsDock::view()
+{
+    return d->view;
+}
+
+QAbstractItemModel* ComponentsDock::model()
+{
+    return d->view->model();
+}
+
---
+++ creator/plugins/docks/componentsdock/componentsdockplugin.cpp
@@ -0,0 +1,42 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "componentsdockplugin.h"
+
+#include "componentsdock.h"
+
+#include <gluoncreatorlib_macros.h>
+#include <KLocalizedString>
+
+using namespace Gluon::Creator;
+
+
+Gluon::Creator::ComponentsDockPlugin::ComponentsDockPlugin(QObject* parent, const QList< QVariant >& params) : DockPlugin(parent, params)
+{
+
+}
+
+Gluon::Creator::ComponentsDockPlugin::~ComponentsDockPlugin()
+{
+
+}
+
+Dock* Gluon::Creator::ComponentsDockPlugin::createDock(KXmlGuiWindow* parent)
+{
+    return new ComponentsDock(i18n("Components"), parent);
+}
+
+GLUON_CREATOR_PLUGIN_EXPORT(ComponentsDockPlugin)
