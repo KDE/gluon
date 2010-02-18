515f593dafced4ee145b0766836d28d83b7e60fb
Dan Leinir Turthra Jensen <admin@leinir.dk>
2010-02-18 19:47:41 +0100

    Add the new property widget item for GluonObject references...

---
+++ creator/lib/widgets/gluonobjectpropertywidgetitem.cpp
@@ -0,0 +1,73 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "gluonobjectpropertywidgetitem.h"
+#include "core/gluonobject.h"
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QLabel>
+#include <QtGui/QPushButton>
+
+Q_DECLARE_METATYPE(GluonCore::GluonObject*);
+
+using namespace GluonCreator;
+
+GluonObjectPropertyWidgetItem::GluonObjectPropertyWidgetItem(QWidget* parent, Qt::WindowFlags f)
+    : PropertyWidgetItem(parent, f)
+{
+    QHBoxLayout * base = new QHBoxLayout(this);
+    base->setSpacing(0);
+    base->setContentsMargins(0, 0, 0, 0);
+
+    currentValue = new QLabel(this);
+    base->addWidget(currentValue);
+    
+    browseButton = new QPushButton(this);
+    browseButton->setText("...");
+    base->addWidget(browseButton);
+    
+    setLayout(base);
+}
+
+GluonObjectPropertyWidgetItem::~GluonObjectPropertyWidgetItem()
+{
+}
+
+PropertyWidgetItem*
+GluonObjectPropertyWidgetItem::instantiate()
+{
+    return new GluonObjectPropertyWidgetItem();
+}
+
+QList< QString >
+GluonObjectPropertyWidgetItem::supportedDataTypes() const
+{
+    QList<QString> supportedTypes;
+    return supportedTypes;
+}
+
+void
+GluonObjectPropertyWidgetItem::setEditValue(const QVariant& value)
+{
+    PropertyWidgetItem::setEditValue(value);
+}
+
+void
+GluonObjectPropertyWidgetItem::objectValueChanged(GluonCore::GluonObject * value)
+{
+    PropertyWidgetItem::valueChanged(QVariant::fromValue<GluonCore::GluonObject*>(value));
+}
+
+#include "gluonobjectpropertywidgetitem.moc"
---
+++ creator/lib/widgets/gluonobjectpropertywidgetitem.h
@@ -0,0 +1,48 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef GLUONCREATOR_GLUONOBJECTPROPERTYWIDGETITEM_H
+#define GLUONCREATOR_GLUONOBJECTPROPERTYWIDGETITEM_H
+
+#include <../../home/leinir/Documents/Uni/msc/gluon/creator/lib/widgets/propertywidgetitem.h>
+
+class QPushButton;
+class QLabel;
+namespace GluonCore { class GluonObject; }
+
+namespace GluonCreator
+{
+    class GluonObjectPropertyWidgetItem : public PropertyWidgetItem
+    {
+        Q_OBJECT
+        public:
+            GluonObjectPropertyWidgetItem(QWidget* parent = 0, Qt::WindowFlags f = 0);
+            ~GluonObjectPropertyWidgetItem();
+            
+            virtual PropertyWidgetItem* instantiate();
+            virtual QList< QString > supportedDataTypes() const;
+            
+        public slots:
+            void setEditValue(const QVariant& value);
+            void objectValueChanged(GluonCore::GluonObject * value);
+            
+        private:
+            QLabel * currentValue;
+            QPushButton * browseButton;
+    };
+}
+
+#endif // GLUONCREATOR_GLUONOBJECTPROPERTYWIDGETITEM_H
