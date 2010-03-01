cc4350f12a4e80decf5034bf212753c011dd13ec
Arjen-Wander Hiemstra <ahiemstra@ahiemstra-home.sshunet.nl>
2010-03-01 03:46:39 +0100

    Some fixes to the invaders project

--- player/examples/invaders/Assets/Invaders.menu-js.menu.js
+++ player/examples/invaders/Assets/Invaders.menu-js.menu.js
@@ -1,5 +1,9 @@
-var color = 255;
-var increase = false;
+var color = 50;
+var color_increment = 3;
+var ypos = 0.0;
+var ypos_increment = 1 - Math.random() * 2;
+var xpos = 0.0;
+var xpos_increment = 1 - Math.random() * 2;
 
 function start() {
   
@@ -9,24 +13,29 @@ function update() {
   if(GameObject.Key_Space.isActionHeld()) {
     Game.setCurrentScene("Invaders.Game");
   }
+
+  GameObject.setPosition(xpos, ypos, 1);
+  
+  ypos += ypos_increment;
+  if(ypos <= -30 || ypos >= 30) {
+    ypos_increment = -ypos_increment;
+  }
+
+  xpos += xpos_increment;
+  if(xpos <= -30 || xpos >= 30) {
+    xpos_increment = -xpos_increment;
+  }
+
+  GameObject.SpriteRenderer.setColor(color, color, color, 255);
+
+  color += color_increment;
+  if(color <= 50 || color >= 250) {
+    color_increment = -color_increment;
+  }
 }
 
 function draw(time) {
-  GameObject.SpriteRenderer.setColor(color, color, color, 255);
 
-  if(increase) {
-    color += 10;
-    if(color >= 250) {
-      increase = false;
-    }
-  }
-  else
-  {
-    color -= 10;
-    if(color < 0) {
-      increase = true;
-    }
-  }
 }
 
 function stop() {
