62255fd558bf7c9d751bc969104c2d51e7fb2614
Dan Leinir Turthra Jensen <admin@leinir.dk>
2010-03-14 14:48:04 +0100

    Add QFont PWI (mmm, KFontRequester ;) )

---
+++ creator/plugins/docks/propertiesdock/propertywidgetitems/qfontpropertywidgetitem.cpp
@@ -0,0 +1,64 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "qfontpropertywidgetitem.h"
+
+#include <KFontRequester>
+
+REGISTER_PROPERTYWIDGETITEM(GluonCreator,QFontPropertyWidgetItem);
+
+using namespace GluonCreator;
+
+QFontPropertyWidgetItem::QFontPropertyWidgetItem(QWidget* parent, Qt::WindowFlags f)
+    : PropertyWidgetItem(parent, f)
+{
+    KFontRequester * theFontReq = new KFontRequester(this);
+    connect(theFontReq, SIGNAL(fontSelected(const QFont&)), this, SLOT(fontChanged(const QFont&)));
+    setEditWidget(theFontReq);
+}
+
+QFontPropertyWidgetItem::~QFontPropertyWidgetItem()
+{
+}
+
+QList< QString >
+QFontPropertyWidgetItem::supportedDataTypes() const
+{
+    QList<QString> supportedTypes;
+    supportedTypes.append("QFont");
+    return supportedTypes;
+}
+
+GluonCreator::PropertyWidgetItem*
+QFontPropertyWidgetItem::instantiate()
+{
+    return new QFontPropertyWidgetItem();
+}
+
+void
+QFontPropertyWidgetItem::setEditValue(const QVariant& value)
+{
+    editWidget()->setProperty("font", value);
+    GluonCreator::PropertyWidgetItem::setEditValue(value);
+}
+
+void QFontPropertyWidgetItem::fontChanged(const QFont& value)
+{
+    PropertyWidgetItem::valueChanged(QVariant(value));
+}
+
+
+#include "qfontpropertywidgetitem.moc"
---
+++ creator/plugins/docks/propertiesdock/propertywidgetitems/qfontpropertywidgetitem.h
@@ -0,0 +1,40 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef GLUONCREATOR_QFONTPROPERTYWIDGETITEM_H
+#define GLUONCREATOR_QFONTPROPERTYWIDGETITEM_H
+
+#include "widgets/propertywidgetitem.h"
+
+namespace GluonCreator
+{
+    class QFontPropertyWidgetItem : public GluonCreator::PropertyWidgetItem
+    {
+        Q_OBJECT
+        public:
+            QFontPropertyWidgetItem(QWidget* parent = 0, Qt::WindowFlags f = 0);
+            ~QFontPropertyWidgetItem();
+            
+            virtual GluonCreator::PropertyWidgetItem* instantiate();
+            virtual QList< QString > supportedDataTypes() const;
+
+        public slots:
+            virtual void setEditValue(const QVariant& value);
+            void fontChanged(const QFont& value);
+    };
+}
+
+#endif // GLUONCREATOR_QFONTPROPERTYWIDGETITEM_H
