69f914be7fbafd96531846cbabb365bc33544dac
Arjen-Wander Hiemstra <ahiemstra@ahiemstra-home.sshunet.nl>
2010-03-19 18:03:45 +0100

    Add some texture coordinates. Fixes texture problems.

--- graphics/meshes/spritemesh.cpp
+++ graphics/meshes/spritemesh.cpp
@@ -46,18 +46,26 @@ SpriteMesh::setSize(const QSizeF &size)
     Vertex *vertex = vertexAt(0);
     vertex->setX(-halfw);
     vertex->setY(-halfh);
+    vertex->setTex(QVector2D(0, 1));
+    vertex->setColor(Qt::white);
 
     vertex = vertexAt(1);
     vertex->setX(-halfw);
     vertex->setY(halfh);
+    vertex->setTex(QVector2D(0, 0));
+    vertex->setColor(Qt::white);
 
     vertex = vertexAt(2);
     vertex->setX(halfw);
     vertex->setY(halfh);
+    vertex->setTex(QVector2D(1, 0));
+    vertex->setColor(Qt::white);
 
     vertex = vertexAt(3);
     vertex->setX(halfw);
     vertex->setY(-halfh);
+    vertex->setTex(QVector2D(1, 1));
+    vertex->setColor(Qt::white);
 }
 
 QSizeF GluonGraphics::SpriteMesh::size() const
