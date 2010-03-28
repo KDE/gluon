32fc1ece0196c7e1fb0bb9363b6ecf9619ca3d58
Dan Leinir Turthra Jensen <admin@leinir.dk>
2010-03-28 18:36:30 +0200

    HUEG COMMIT AM HUEG! Alright, gone through every cpp and h file in the entire project, replacing the erroneous headers, and adding where none existed. Please, everybody, could you go through the files and add/remove yourself where applicable?

--- core/debughelper.h
+++ core/debughelper.h
@@ -1,6 +1,23 @@
-/*
- * code by wysota - http://blog.wysota.eu.org/index.php/2009/11/17/little-debugging-helper/
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2009 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
+// Adapted from code by wysota - http://blog.wysota.eu.org/index.php/2009/11/17/little-debugging-helper/
 
 #ifndef GLUON_CORE_DEBUGHELPER_H
 #define GLUON_CORE_DEBUGHELPER_H
