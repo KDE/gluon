59dcacbd98aee39640bb4beafb0c860408325589
Arjen Hiemstra <ahiemstra@heimr.nl>
2010-04-03 17:40:40 +0200

    Add support for collisions and start with clone() support

---
+++ player/examples/invaders/Assets/invaders_bullet.js
@@ -0,0 +1,5 @@
+
+function update(time)
+{
+    GameObject.translate(0, -20*(time/1000), 0);
+}
--- player/examples/invaders/Assets/invaders_player.js
+++ player/examples/invaders/Assets/invaders_player.js
@@ -1,30 +1,64 @@
-
-function start() {
-  
+function start() 
+{
 }
 
-function update(time) {
-  if(GameObject.Key_Left.isActionHeld())
-  {
-    GameObject.translate(10*(time/1000), 0, 0);
-  }
+function update(time) 
+{
+    if(GameObject.Key_Left.isActionHeld())
+    {
+        GameObject.translate(10*(time/1000), 0, 0);
+    }
 
-  if(GameObject.Key_Right.isActionHeld())
-  {
-    GameObject.translate(-10*(time/1000), 0, 0);
-  }
+    if(GameObject.Key_Right.isActionHeld())
+    {
+        GameObject.translate(-10*(time/1000), 0, 0);
+    }
 
-  var pos = GameObject.position;
-  if(pos.x() < -30)
-    GameObject.setPosition(-30, pos.y(), pos.z());
-  if(pos.x() > 30)
-    GameObject.setPosition(30, pos.y(), pos.z());
+    if(GameObject.Key_Up.isActionHeld())
+    {
+        GameObject.translate(0, 10*(time/1000), 0);
+    }
+
+    if(GameObject.Key_Down.isActionHeld())
+    {
+        GameObject.translate(0, -10*(time/1000), 0);
+    }
+
+    var pos = GameObject.position;
+    if(pos.x() < -30)
+        GameObject.setPosition(-30, pos.y(), pos.z());
+    if(pos.x() > 30)
+        GameObject.setPosition(30, pos.y(), pos.z());
+    if(pos.y() > 30)
+        GameObject.setPosition(pos.x(), 30, pos.z());
+    if(pos.y() < -30)
+        GameObject.setPosition(pos.x(), -30, pos.z());
+    
+    if(GameObject.Collider.isColliding())
+    {
+        GameObject.SpriteRenderer.setColor(new QColor(255, 0, 0));
+    }
+    else
+    {
+        GameObject.SpriteRenderer.setColor(new QColor(255, 255, 255));
+    }
+    
+    if(GameObject.Key_Fire.isActionHeld())
+    {
+        GameObject.FireSound.play();
+        
+        var bullet = Game.clone(Game.getFromScene("Bullet"));
+        bullet.setPosition(GameObject.position);
+        bullet.Script.enabled = true;
+    }
 }
 
-function draw() {
-  
+function draw() 
+{
+
 }
 
-function stop() {
-  
+function stop() 
+{
+
 }
