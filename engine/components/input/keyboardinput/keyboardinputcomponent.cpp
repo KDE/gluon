b61ad9e65e42947ee1b9c66bb82074e70224bfd0
Arjen Hiemstra <ahiemstra@heimr.nl>
2010-04-03 21:20:05 +0200

    Try to avoid crashing

--- engine/components/input/keyboardinput/keyboardinputcomponent.cpp
+++ engine/components/input/keyboardinput/keyboardinputcomponent.cpp
@@ -7,12 +7,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -80,7 +80,9 @@ KeyboardInputComponent::update(int elapsedMilliseconds)
 
 void KeyboardInputComponent::stop()
 {
-    m_keyboard->setEnabled(false);
+    if(m_keyboard) {
+        m_keyboard->setEnabled(false);
+    }
 
     m_actionStopped = false;
     m_actionStarted = false;
