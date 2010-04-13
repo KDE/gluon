5faeefb97ef0b2eb6ce08ff9b1f98831344ce6fa
Dan Leinir Turthra Jensen <admin@leinir.dk>
2010-04-13 16:50:41 +0200

    Make the enemy death animation work properly, yay! Now the explosion sound plays fully as well :)

--- player/examples/invaders/Assets/invaders_enemy.js
+++ player/examples/invaders/Assets/invaders_enemy.js
@@ -5,39 +5,59 @@ var controller = null;
 var bullet = null;
 var bulletChance = 0.005;
 var alive = true;
+var deathAnimTime = 439; // The length of the quiet explosion sound
+var deathAnimTimeRemaining = 0;
 
 function start() {
 	controller = Game.getFromScene("Background").ControllerScript;
     bullet = Game.getFromScene("Bullet");
+	deathAnimTimeRemaining = deathAnimTime;
 }
 
 function update(time) {
 	if(controller.paused)
 		return;
 
-    GameObject.translate(xdir * (time/1000), 0, 0);
-    
-    var position = GameObject.position;
-    if(position.x() > 35 || position.x() < -35) {
-        GameObject.translate(0, ydir, 0);
-        xdir = -xdir;
-    }
-    
-    if(GameObject.BulletCollider.isColliding())
-    {
-		alive = false;
-        GameObject.destroy();
-        GameObject.BulletCollider.collidesWith().destroy();
-        GameObject.Kapow.play();
-        Game.enemies--;
-    }
-    
-    if(Math.random() < bulletChance)
-    {
-        var newBullet = Game.clone(bullet);
-        newBullet.position = GameObject.position;
-        newBullet.enabled = true;
-        newBullet.Collider.collisionGroup = 0;
-        newBullet.rotate(180, new QVector3D(0, 0, 1));
-    }
+	if(alive)
+	{
+		GameObject.translate(xdir * (time/1000), 0, 0);
+		
+		var position = GameObject.position;
+		if(position.x() > 35 || position.x() < -35) {
+			GameObject.translate(0, ydir, 0);
+			xdir = -xdir;
+		}
+		
+		if(GameObject.BulletCollider.isColliding())
+		{
+			alive = false;
+			GameObject.BulletCollider.collidesWith().destroy();
+			GameObject.Kapow.play();
+			Game.enemies--;
+		}
+		
+		if(Math.random() < bulletChance)
+		{
+			var newBullet = Game.clone(bullet);
+			newBullet.position = GameObject.position;
+			newBullet.enabled = true;
+			newBullet.Collider.collisionGroup = 0;
+			newBullet.rotate(180, new QVector3D(0, 0, 1));
+		}
+	}
+	else
+	{
+		var scaleVal = 1 + (deathAnimTimeRemaining / deathAnimTime);
+		GameObject.setScale(new QVector3D(scaleVal, scaleVal, 1));
+		
+		var color = GameObject.SpriteRenderer.color;
+		color.setAlpha(255 * (scaleVal - 1));
+		GameObject.SpriteRenderer.setColor(color);
+		
+		deathAnimTimeRemaining = deathAnimTimeRemaining - time;
+
+		if(deathAnimTimeRemaining < 0)
+			GameObject.destroy();
+	}
+
 }
