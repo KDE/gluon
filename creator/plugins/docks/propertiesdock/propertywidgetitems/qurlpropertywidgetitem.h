9c077866f4d2c663ab4bbd7e3a8621185be39003
Dan Leinir Turthra Jensen <admin@leinir.dk>
2010-06-17 10:53:53 +0200

    Code style++

--- creator/plugins/docks/propertiesdock/propertywidgetitems/qurlpropertywidgetitem.h
+++ creator/plugins/docks/propertiesdock/propertywidgetitems/qurlpropertywidgetitem.h
@@ -1,46 +1,43 @@
-/*
-    <one line to give the library's name and an idea of what it does.>
-    Copyright (C) <year>  <name of author>
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-*/
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2010 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #ifndef GLUONCREATOR_QURLPROPERTYWIDGETITEM_H
 #define GLUONCREATOR_QURLPROPERTYWIDGETITEM_H
 
 #include "widgets/propertywidgetitem.h"
 
-
-namespace GluonCreator {
-
-class QUrlPropertyWidgetItem : public GluonCreator::PropertyWidgetItem
+namespace GluonCreator
 {
-    Q_OBJECT
-    public:
-        explicit QUrlPropertyWidgetItem(QWidget* parent = 0, Qt::WindowFlags f = 0);
-        ~QUrlPropertyWidgetItem();
+    class QUrlPropertyWidgetItem : public PropertyWidgetItem
+    {
+            Q_OBJECT
+        public:
+            explicit QUrlPropertyWidgetItem(QWidget* parent = 0, Qt::WindowFlags f = 0);
+            ~QUrlPropertyWidgetItem();
 
-        virtual GluonCreator::PropertyWidgetItem* instantiate();
-        virtual QList< QString > supportedDataTypes() const;
+            virtual GluonCreator::PropertyWidgetItem* instantiate();
+            virtual QList< QString > supportedDataTypes() const;
 
-    public slots:
-        virtual void setEditValue(const QVariant& value);
-        void urlValueChanged();
-};
-
+        public slots:
+            virtual void setEditValue(const QVariant& value);
+            void urlValueChanged();
+    };
 }
 
 #endif // GLUONCREATOR_QURLPROPERTYWIDGETITEM_H
