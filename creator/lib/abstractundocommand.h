feb662fd7ff6a3535bbd966832aa413381761cf4
Arjen Hiemstra <ahiemstra@heimr.nl>
2010-08-07 23:09:22 +0200

    Add an AbstractUndoCommand class that will serve as base class for undo commands

---
+++ creator/lib/abstractundocommand.h
@@ -0,0 +1,57 @@
+/*****************************************************************************
+* This file is part of the Gluon Development Platform
+* Copyright (c) 2010 Arjen Hiemstra <ahiemstra@heimr.nl>
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef GLUONCREATOR_ABSTRACTUNDOCOMMAND_H
+#define GLUONCREATOR_ABSTRACTUNDOCOMMAND_H
+
+#include <QUndoStack>
+
+#include "gluoncreator_macros.h"
+
+namespace GluonCore
+{
+    class GluonObject;
+}
+
+namespace GluonCreator {
+
+    class GLUONCREATOR_EXPORT AbstractUndoCommand : public QUndoCommand
+    {
+        public:
+            AbstractUndoCommand(QUndoCommand* parent = 0);
+            virtual ~AbstractUndoCommand();
+
+            virtual void undo() = 0;
+            virtual void redo() = 0;
+            virtual int id() const;
+
+            GluonCore::GluonObject * object() const;
+            QString commandName() const;
+
+        protected:
+            void setObject(GluonCore::GluonObject* obj);
+            void setCommandName(const QString& name);
+
+        private:
+            class AbstractUndoCommandPrivate;
+            AbstractUndoCommandPrivate * const d;
+    };
+}
+
+#endif // GLUONCREATOR_ABSTRACTUNDOCOMMAND_H
