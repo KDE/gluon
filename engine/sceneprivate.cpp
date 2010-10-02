2947fc6b60d5158d327c733ee64e51ca58330ff7
Arjen Hiemstra <ahiemstra@heimr.nl>
2010-10-02 18:51:28 +0200

    Debug--;

--- engine/sceneprivate.cpp
+++ engine/sceneprivate.cpp
@@ -7,12 +7,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -56,10 +56,9 @@ ScenePrivate::unloadContents()
 void
 ScenePrivate::loadContents(const QUrl& file)
 {
-    DEBUG_FUNC_NAME
+    DEBUG_BLOCK
     if (file.isEmpty())
     {
-        DEBUG_TEXT(QString("Filename for scene was empty, so we can't load - aborting"));
         sceneContentsLoaded = true;
         return;
     }
