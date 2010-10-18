a2a3a7d6bc83d0da88c5642a7eb250de3cb12ac2
Andreas Marschke <xxtjaxx@gmail.com>
2010-10-18 03:10:59 +0200

    player/plasmoid/plasmaapplet.cpp: borked the build because <gl.h> couldnt befound.

--- player/plasmoid/plasmaapplet.cpp
+++ player/plasmoid/plasmaapplet.cpp
@@ -42,7 +42,7 @@
 #include <QGraphicsSceneResizeEvent>
 #include <QGraphicsLinearLayout>
 
-#include <gl.h>
+// #include <gl.h>
 
 using namespace GluonPlayer;
 using namespace GluonGraphics;
@@ -51,152 +51,152 @@ PlasmaApplet::PlasmaApplet(QObject* parent, const QVariantList& args) : GLFBOApp
 	m_viewportWidth(0), m_viewportHeight(0), m_project(0), m_camera(0),
 	m_gamesOverlay(0), m_gameDetailsOverlay(0)
 {
-    setHasConfigurationInterface(true);
-    setAspectRatioMode(Plasma::IgnoreAspectRatio);
-    setBackgroundHints(Plasma::Applet::NoBackground);
-    setMinimumSize(200, 200);
-    resize(500, 500);
+	setHasConfigurationInterface(true);
+	setAspectRatioMode(Plasma::IgnoreAspectRatio);
+	setBackgroundHints(Plasma::Applet::NoBackground);
+	setMinimumSize(200, 200);
+	resize(500, 500);
 }
 
 PlasmaApplet::~PlasmaApplet()
 {
-    kDebug() << "QUIT";
-    if (m_gameFileName.isEmpty()) {
+	kDebug() << "QUIT";
+	if (m_gameFileName.isEmpty()) {
 	return;
-    }
-    GluonEngine::Game::instance()->stopGame();
-    GluonEngine::Game::instance()->cleanupAll();
+	}
+	GluonEngine::Game::instance()->stopGame();
+	GluonEngine::Game::instance()->cleanupAll();
 }
 
 void PlasmaApplet::init()
 {
-    m_gamesModel = new GamesModel(this);
-    m_layout = new QGraphicsLinearLayout(Qt::Vertical);
-    setLayout(m_layout);
-    showGames();
+	m_gamesModel = new GamesModel(this);
+	m_layout = new QGraphicsLinearLayout(Qt::Vertical);
+	setLayout(m_layout);
+	showGames();
 }
 
 void PlasmaApplet::setProject(const QModelIndex &index)
 {
-    m_gameFileName = index.data().toString();
-    m_gamesOverlay->hide();
-    openProject();
+	m_gameFileName = index.data().toString();
+	m_gamesOverlay->hide();
+	openProject();
 }
 
 void PlasmaApplet::openProject()
 {
-    if (m_gameFileName.isEmpty()) {
+	if (m_gameFileName.isEmpty()) {
 	return;
-    }
+	}
 
-    setBusy(true);
+	setBusy(true);
 
-    initGL();
-    GluonCore::GluonObjectFactory::instance()->loadPlugins();
+	initGL();
+	GluonCore::GluonObjectFactory::instance()->loadPlugins();
 
-    m_project = new GluonEngine::GameProject();
-    m_project->loadFromFile(QUrl(m_gameFileName));
-    GluonEngine::Game::instance()->setGameProject(m_project);
-    GluonEngine::Game::instance()->setCurrentScene(m_project->entryPoint());
+	m_project = new GluonEngine::GameProject();
+	m_project->loadFromFile(QUrl(m_gameFileName));
+	GluonEngine::Game::instance()->setGameProject(m_project);
+	GluonEngine::Game::instance()->setCurrentScene(m_project->entryPoint());
 
-    connect(GluonEngine::Game::instance(), SIGNAL(painted(int)), this, SLOT(doPaint()));
-    QTimer::singleShot(1000, this, SLOT(startGame()));
+	connect(GluonEngine::Game::instance(), SIGNAL(painted(int)), this, SLOT(doPaint()));
+	QTimer::singleShot(1000, this, SLOT(startGame()));
 }
 
 void PlasmaApplet::doPaint()
 {
-    makeCurrent();
-    update();
+	makeCurrent();
+	update();
 }
 
 void PlasmaApplet::startGame()
 {
-    setBusy(false);
-    GluonEngine::Game::instance()->runGame();
+	setBusy(false);
+	GluonEngine::Game::instance()->runGame();
 }
 
 void PlasmaApplet::initGL()
 {
-    m_camera = Engine::instance()->activeCamera();
-    connect(Engine::instance(), SIGNAL(activeCameraChanged(GluonGraphics::Camera*)),
-	    SLOT(setCamera(GluonGraphics::Camera*)));
+	m_camera = Engine::instance()->activeCamera();
+	connect(Engine::instance(), SIGNAL(activeCameraChanged(GluonGraphics::Camera*)),
+		SLOT(setCamera(GluonGraphics::Camera*)));
 
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glShadeModel(GL_SMOOTH);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);
-    glEnable(GL_SCISSOR_TEST);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glEnable(GL_SCISSOR_TEST);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void PlasmaApplet::resizeEvent(QGraphicsSceneResizeEvent* event)
 {
-    m_viewportWidth = event->newSize().width();
-    m_viewportHeight = event->newSize().height();
+	m_viewportWidth = event->newSize().width();
+	m_viewportHeight = event->newSize().height();
 
-    glViewport(0, 0, m_viewportWidth, m_viewportHeight);
+	glViewport(0, 0, m_viewportWidth, m_viewportHeight);
 }
 
 void PlasmaApplet::showGames()
 {
-    if (!m_gamesOverlay) {
+	if (!m_gamesOverlay) {
 	m_gamesOverlay = new GamesOverlay(this);
 	m_gamesOverlay->gamesView()->setModel(m_gamesModel);
 	m_gamesOverlay->setGeometry(geometry());
 	connect(m_gamesOverlay, SIGNAL(gameToPlaySelected(QModelIndex)), SLOT(setProject(QModelIndex)));
 	connect(m_gamesOverlay, SIGNAL(gameSelected(QModelIndex)), SLOT(showGameDetails(QModelIndex)));
-    }
+	}
 
-    if (m_gameDetailsOverlay) {
+	if (m_gameDetailsOverlay) {
 	m_gameDetailsOverlay->hide();
 	m_layout->removeItem(m_gameDetailsOverlay);
 	m_gameDetailsOverlay->deleteLater();
 	m_gameDetailsOverlay = 0;
-    }
+	}
 
-    m_layout->addItem(m_gamesOverlay);
-    m_gamesOverlay->show();
+	m_layout->addItem(m_gamesOverlay);
+	m_gamesOverlay->show();
 }
 
 void PlasmaApplet::showGameDetails(const QModelIndex& index)
 {
 	Q_UNUSED(index);
 
-    //TODO: the game details should be according to the game selected
-    m_gameDetailsOverlay = new GameDetailsOverlay(this);
-    m_gamesOverlay->hide();
-    m_layout->removeItem(m_gamesOverlay);
-    m_gameDetailsOverlay->show();
-    m_layout->addItem(m_gameDetailsOverlay);
-    connect(m_gameDetailsOverlay, SIGNAL(back()), SLOT(showGames()));
+	//TODO: the game details should be according to the game selected
+	m_gameDetailsOverlay = new GameDetailsOverlay(this);
+	m_gamesOverlay->hide();
+	m_layout->removeItem(m_gamesOverlay);
+	m_gameDetailsOverlay->show();
+	m_layout->addItem(m_gameDetailsOverlay);
+	connect(m_gameDetailsOverlay, SIGNAL(back()), SLOT(showGames()));
 }
 
 void PlasmaApplet::setCamera(Camera* camera)
 {
-    m_camera = camera;
+	m_camera = camera;
 }
 
 void PlasmaApplet::render()
 {
-    Engine::instance()->render();
+	Engine::instance()->render();
 }
 
 void PlasmaApplet::paintGLInterface(QPainter* painter, const QStyleOptionGraphicsItem* option)
 {
-    Q_UNUSED(painter);
-    Q_UNUSED(option);
+	Q_UNUSED(painter);
+	Q_UNUSED(option);
 
-    glScissor(0, 0, 400, 400);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+	glScissor(0, 0, 400, 400);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
 
-    Engine::instance()->render();
+	Engine::instance()->render();
 
-    glPushAttrib(GL_ALL_ATTRIB_BITS);
-    render();
-    glPopAttrib();
+	glPushAttrib(GL_ALL_ATTRIB_BITS);
+	render();
+	glPopAttrib();
 }
 
 #include "plasmaapplet.moc"
