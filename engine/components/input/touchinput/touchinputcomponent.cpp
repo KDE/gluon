1e6cce7af2a272205ae4c751b725ddcb8449d045
Laszlo Papp <djszapi@archlinux.us>
2010-10-26 01:09:13 -0700

    Add Touch input component to the project

---
+++ engine/components/input/touchinput/touchinputcomponent.cpp
@@ -0,0 +1,124 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (C) 2010 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ * Copyright (c) 2010 Arjen Hiemstra <ahiemstra@heimr.nl>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#include "touchinputcomponent.h"
+#include "input/inputmanager.h"
+
+#include <core/debughelper.h>
+
+#include <QtCore/QEvent>
+#include <QtCore/QDebug>
+
+REGISTER_OBJECTTYPE(GluonEngine, TouchInputComponent);
+
+using namespace GluonEngine;
+
+TouchInputComponent::TouchInputComponent(QObject *parent)
+    : Component(parent)
+    , m_actionHeld(false)
+    , m_actionStarted(false)
+    , m_actionStopped(false)
+    , m_touch(0)
+{
+}
+
+QString TouchInputComponent::category() const
+{
+    return QString("Input");
+}
+
+void TouchInputComponent::initialize()
+{
+    if (!m_touch)
+        m_touch = GluonInput::InputManager::instance()->touch();
+}
+
+void TouchInputComponent::start()
+{
+    if (m_touch) {
+        m_touch->setEnabled(true);
+    } else {
+        debug("WARNING! No touch found!");
+    }
+}
+
+void TouchInputComponent::update(int elapsedMilliseconds)
+{
+    DEBUG_BLOCK
+    if (m_actionStarted)
+        m_actionStarted = false;
+
+    if (m_actionStopped)
+        m_actionStopped = false;
+
+    if (m_touch && m_touch->buttonPressed(m_touchCode)) {
+        if (!m_actionHeld) {
+            m_actionStarted = true;
+            m_actionHeld = true;
+        }
+    } else {
+        if (m_actionHeld) {
+            m_actionStopped = true;
+            m_actionHeld = false;
+        }
+    }
+}
+
+void TouchInputComponent::stop()
+{
+    if (m_touch) {
+        m_touch->setEnabled(false);
+    }
+
+    m_actionStopped = false;
+    m_actionStarted = false;
+    m_actionHeld = false;
+}
+
+bool TouchInputComponent::isActionStarted()
+{
+    return m_actionStarted;
+}
+
+bool TouchInputComponent::isActionHeld()
+{
+    return m_actionHeld;
+}
+
+bool TouchInputComponent::isActionStopped()
+{
+    return m_actionStopped;
+}
+
+TouchInputComponent::TouchName
+TouchInputComponent::touchCode() const
+{
+    return m_touchCode;
+}
+
+void TouchInputComponent::setTouchCode(TouchInputComponent::TouchName newTouchCode)
+{
+    m_touchCode = newTouchCode;
+}
+
+Q_EXPORT_PLUGIN2(gluon_component_touchinput, GluonEngine::TouchInputComponent);
+
+#include "touchinputcomponent.moc"
---
+++ engine/components/input/touchinput/touchinputcomponent.h
@@ -0,0 +1,85 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (C) 2010 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ * Copyright (c) 2010 Arjen Hiemstra <ahiemstra@heimr.nl>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#ifndef TOUCHINPUTCOMPONENT_H
+#define TOUCHINPUTCOMPONENT_H
+
+#include "component.h"
+
+#include "gluonbuttons.h"
+
+namespace GluonInput {
+    class Touch;
+}
+
+namespace GluonEngine
+{
+    class TouchInputComponent : public Component
+    {
+            Q_OBJECT;
+            GLUON_OBJECT(GluonEngine::TouchInputComponent)
+            Q_PROPERTY(TouchName touchCode READ touchCode WRITE setTouchCode);
+            Q_INTERFACES(GluonEngine::Component)
+            Q_ENUMS(KeyName)
+
+        public:
+            enum TouchName
+            {
+                UNKNOWN,
+                ONE,
+            };
+
+            Q_INVOKABLE TouchInputComponent(QObject *parent = 0);
+            virtual QString category() const;
+
+            // True on any frame between getActionStarted and
+            // getActionStopped, inclusive of those two frames
+            Q_INVOKABLE bool isActionHeld();
+            // True on the frame when an action was begun (for example the
+            // button represented by buttonName was pressed on a controller)
+            Q_INVOKABLE bool isActionStarted();
+            // True on the frame when an action was stopped (for example a
+            // button was released on a controller)
+            Q_INVOKABLE bool isActionStopped();
+
+            virtual void initialize();
+            virtual void start();
+            virtual void update(int elapsedMilliseconds);
+            virtual void stop();
+
+            TouchName touchCode() const;
+            void setTouchCode(TouchName newTouchCode);
+
+        private:
+            bool m_actionHeld;
+            bool m_actionStarted;
+            bool m_actionStopped;
+
+            TouchName m_touchCode;
+
+            GluonInput::Touch *m_touch;
+    };
+}
+
+Q_DECLARE_METATYPE(GluonEngine::TouchInputComponent)
+Q_DECLARE_METATYPE(GluonEngine::TouchInputComponent *)
+
+#endif // TOUCHINPUTCOMPONENT_H
