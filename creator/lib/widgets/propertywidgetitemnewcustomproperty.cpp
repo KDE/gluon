142877ff753564fc96e1ac828d2f629900ec4b01
Dan Leinir Turthra Jensen <admin@leinir.dk>
2011-01-10 15:55:32 +0000

    Add support for creating custom properties of the GluonObject types

--- creator/lib/widgets/propertywidgetitemnewcustomproperty.cpp
+++ creator/lib/widgets/propertywidgetitemnewcustomproperty.cpp
@@ -27,6 +27,8 @@
 
 #include <klocalizedstring.h>
 #include <core/gluonobject.h>
+#include <QVector2D>
+#include <QQuaternion>
 
 using namespace GluonCreator;
 
@@ -36,16 +38,34 @@ PropertyWidgetItemNewCustomProperty::PropertyWidgetItemNewCustomProperty(QWidget
     propertyName = new QLineEdit(this);
 
     propertyType = new QComboBox(this);
-    propertyType->addItem(ki18n("Boolean (bool)").toString(), QVariant::fromValue<bool>(false));
-    propertyType->addItem(ki18n("Integer (int)").toString(), QVariant::fromValue<int>(0));
-    propertyType->addItem(ki18n("Color (QColor)").toString(), QVariant::fromValue<QColor>(QColor()));
-    propertyType->addItem(ki18n("Double precision floating point (double)").toString(), QVariant::fromValue<double>(0));
-    propertyType->addItem(ki18n("Floating point (float)").toString(), QVariant::fromValue<float>(0));
-//    propertyType->addItem(ki18n("").toString(), QString(""));
+    propertyType->addItem(i18n("Boolean (bool)"), QVariant::fromValue<bool>(false));
+    propertyType->addItem(i18n("Integer (int)"), QVariant::fromValue<int>(0));
+    propertyType->addItem(i18n("Color (rgba)"), QVariant::fromValue<QColor>(QColor()));
+    propertyType->addItem(i18n("Double precision floating point (double)"), QVariant::fromValue<double>(0));
+    propertyType->addItem(i18n("Floating point (float)"), QVariant::fromValue<float>(0));
+    propertyType->addItem(i18n("Large integer (longlong)"), QVariant::fromValue<qlonglong>(0));
+    propertyType->addItem(i18n("String (string)"), QVariant::fromValue<QString>(""));
+    propertyType->addItem(i18n("2D Vector (vector2d)"), QVariant::fromValue<QVector2D>(QVector2D()));
+    propertyType->addItem(i18n("3D Vector (vector3d)"), QVariant::fromValue<QVector3D>(QVector3D()));
+    propertyType->addItem(i18n("4D Vector (vector4d)"), QVariant::fromValue<QVector4D>(QVector4D()));
+    propertyType->addItem(i18n("Quaternion (quaternion)"), QVariant::fromValue<QQuaternion>(QQuaternion()));
+    propertyType->addItem(i18n("Unsigned integer (uint)"), QVariant::fromValue<uint>(0));
+    propertyType->addItem(i18n("Unsigned large integer (ulonglong)"), QVariant::fromValue<qulonglong>(0));
+    propertyType->addItem(i18n("URL (url)"), QVariant::fromValue<QUrl>(QUrl()));
+    propertyType->addItem(i18n("2D size (size2d)"), QVariant::fromValue<QSizeF>(QSizeF()));
+    QHash<QString, const QMetaObject* > objectTypes = GluonCore::GluonObjectFactory::instance()->objectTypes();
+    QHash<QString, const QMetaObject* >::const_iterator i;
+    for(i = objectTypes.constBegin(); i != objectTypes.constEnd(); ++i)
+    {
+        QObject* obj = i.value()->newInstance();
+        GluonCore::GluonObject* gObj = qobject_cast< GluonCore::GluonObject* >(obj);
+        if(gObj)
+            propertyType->addItem(i.key(), gObj->toVariant(0));
+    }
     
     QFormLayout* formLayout = new QFormLayout();
-    formLayout->addRow(ki18n("Name:").toString(), propertyName);
-    formLayout->addRow(ki18n("Type").toString(), propertyType);
+    formLayout->addRow(i18n("Name:"), propertyName);
+    formLayout->addRow(i18n("Type"), propertyType);
     
     QVBoxLayout* layout = new QVBoxLayout();
     layout->addLayout(formLayout);
