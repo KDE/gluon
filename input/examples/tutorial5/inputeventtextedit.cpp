ba6b72bcea8a042a6f41dd2ebd72cbe25fba6305
Laszlo Papp <djszapi@archlinux.us>
2011-01-20 12:36:48 +0200

    Add the Tutorial5 Input test and example application.

---
+++ input/examples/tutorial5/inputeventtextedit.cpp
@@ -0,0 +1,12 @@
+#include "inputeventtextedit.h"
+
+InputEventTextEdit::InputEventTextEdit( QTextEdit* parent )
+    : QTextEdit( parent )
+{
+}
+
+InputEventTextEdit::~InputEventTextEdit()
+{
+}
+
+#include "inputeventtextedit.moc"
