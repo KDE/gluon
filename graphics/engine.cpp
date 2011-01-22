693b5a1f13fbec9bfe9590d2f7bcf67590adcf2b
Giulio Camuffo <giuliocamuffo@gmail.com>
2011-01-22 13:36:45 +0100

    Connect the default viewport signals.

--- graphics/engine.cpp
+++ graphics/engine.cpp
@@ -373,7 +373,7 @@ Engine::setViewport( Viewport* viewport )
 Engine::Engine()
     : d( new EnginePrivate() )
 {
-    d->viewport = new Viewport();
+    setViewport( new Viewport() );
     connect( this, SIGNAL( activeCameraChanged( Camera* ) ), d->viewport, SLOT( update() ) );
 }
 
