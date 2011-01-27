307fe13ad4f5e43c47b855da457627c2d0bb899c
Giulio Camuffo <giuliocamuffo@gmail.com>
2011-01-27 23:39:44 +0100

    Move the private class "I" from the header to the cpp.

--- engine/game.h
+++ engine/game.h
@@ -37,23 +37,6 @@ namespace GluonEngine
     class Scene;
     class GamePrivate;
 
-    class I : public QThread
-    {
-        public:
-            static void sleep( unsigned long secs )
-            {
-                QThread::sleep( secs );
-            }
-            static void msleep( unsigned long msecs )
-            {
-                QThread::msleep( msecs );
-            }
-            static void usleep( unsigned long usecs )
-            {
-                QThread::usleep( usecs );
-            }
-    };
-
     class GLUON_ENGINE_EXPORT Game : public GluonCore::Singleton<Game>
     {
             Q_OBJECT
