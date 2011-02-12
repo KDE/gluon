eb75d38d8b04ff507c40378359c35146d7d0f078
Laszlo Papp <djszapi@archlinux.us>
2011-02-12 17:48:50 +0100

    KDE Player: Add a GameTextLabel class.
    It was a requirement after thinking of the "clicked" signal management which
    does not really exist for KSqueezedTextLabel, thus a mouseReleaseEvent
    reimplementation was a straight-forward, preferable idea. (=> similar to the fact
    that the gamesviewitem does in the plasmoid player in fact)

---
+++ player/kdeext/views/gametextlabel.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (C) 2010 Laszlo Papp <djszapi@archlinux.us>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef GAMETEXTLABEL_H
+#define GAMETEXTLABEL_H
+
+#include <QtGui/QLabel>
+#include <QtGui/QMouseEvent>
+
+class GameTextLabel : public QLabel
+{
+        Q_OBJECT
+    public:
+        explicit GameTextLabel( QWidget* parent = 0, Qt::WindowFlags wFlags = 0 );
+        virtual ~GameTextLabel();
+
+    protected:
+        void mouseReleaseEvent(QMouseEvent *event);
+
+    signals:
+        void mouseReleased( );
+};
+
+#endif // GAMETEXTLABEL_H
