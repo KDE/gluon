33f763184db5ed5ed675a0a3158689e9fda71cae
Laszlo Papp <djszapi@archlinux.us>
2011-03-10 06:10:12 +0100

    Creator: Add an initial toolplugin class

---
+++ creator/lib/toolplugin.cpp
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2011 Laszlo Papp <djszapi@archlinux.us>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "toolplugin.h"
+#include "toolmanager.h"
+
+#include <KDE/KXmlGuiWindow>
+#include <KDE/KActionCollection>
+#include <KDE/KStandardDirs>
+
+#include <QtGui/QDockWidget>
+
+using namespace GluonCreator;
+
+ToolPlugin::ToolPlugin( QObject* parent, const QList< QVariant >& params ) : Plugin( parent, params )
+{
+
+}
+
+ToolPlugin::~ToolPlugin()
+{
+
+}
+
+void ToolPlugin::load( KXmlGuiWindow* mainWindow )
+{
+    // mainWindow->insertChildClient( this );
+
+    // m_tool = createDock( mainWindow );
+
+    // actionCollection()->addAction( QString( "show%1Action" ).arg( m_tool->objectName() ), m_tool->toggleViewAction() );
+
+    // QString xml = QString( "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kpartgui name=\"gluoncreator_toolkplugin_%1\" version=\"1\">" ).arg( m_tool->objectName() );
+    // xml += QString( "<MenuBar><Menu name=\"settings\"><Menu name=\"tools\"><Action name=\"show%1Action\" /></Menu></Menu></MenuBar></kpartgui>" ).arg( m_tool->objectName() );
+
+    // setXML( xml );
+}
+
+void ToolPlugin::unload( KXmlGuiWindow* /* mainWindow */ )
+{
+    // ToolManager::instance()->removeTool( m_tool );
+    // actionCollection()->removeAction( actionCollection()->action( QString( "show%1Action" ).arg( m_tool->objectName() ) ) );
+    // parentClient()->removeChildClient( this );
+}
---
+++ creator/lib/toolplugin.h
@@ -0,0 +1,47 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2011 Laszlo Papp <djszapi@archlinux.us>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef GLUON_CREATOR_TOOLPLUGIN_H
+#define GLUON_CREATOR_TOOLPLUGIN_H
+
+#include "plugin.h"
+
+class KXmlGuiWindow;
+
+namespace GluonCreator
+{
+
+    class GLUONCREATOR_EXPORT ToolPlugin : public Plugin
+    {
+            Q_OBJECT
+        public:
+            ToolPlugin( QObject* parent, const QList<QVariant>& params );
+            virtual ~ToolPlugin();
+
+            virtual void load( KXmlGuiWindow* mainWindow );
+            virtual void unload( KXmlGuiWindow* mainWindow );
+
+        protected:
+            virtual QDockWidget* createTool( KXmlGuiWindow* parent ) = 0;
+
+    };
+
+}
+
+#endif // GLUON_CREATOR_TOOLPLUGIN_H
