dac2b5f8318012cd78eef367188480a45635a6a1
Laszlo Papp <djszapi@archlinux.us>
2011-03-29 01:24:37 +0200

    Add the new asset to the GameProject, if the parent is an Asset
    CCMAIL: admin@leinir.dk

--- engine/gameproject.cpp
+++ engine/gameproject.cpp
@@ -60,7 +60,10 @@ GameProject::~GameProject()
 void
 GameProject::addChild( GluonObject* child, GluonCore::GluonObject* parent )
 {
-    GluonCore::GluonObject::addChild( child, parent );
+    if( !qobject_cast<GluonEngine::Asset*>(parent) )
+        GluonCore::GluonObject::addChild( child, parent );
+    else
+        GluonCore::GluonObject::addChild( child, 0 );
 
     connect( child, SIGNAL( showDebug( const QString& ) ), Game::instance(), SIGNAL( showDebug( const QString& ) ) );
 }
