a7a11b8193aa929b31461cc501191d9e0c26e3cf
Laszlo Papp <djszapi@archlinux.us>
2011-04-14 08:16:00 +0200

    Fix the scripts up according to the new api and also format changes

--- player/examples/ball.gluon/assets/scripts/player.js
+++ player/examples/ball.gluon/assets/scripts/player.js
@@ -1,35 +1,51 @@
 var gMaxFrameJumpInt = 25;
 var gCurrentlyFrameJumpInt=0
 var gJumpingBool = false;
-function initialize() {
+
+this.initialize = function()
+{
 }
-function update(time) {
+
+this.update = function(time)
+{
     this.rotate();
     this.jump();
 }
-function draw(){
+
+this.draw = function()
+{
 }
-function rotate(time){
-  this.GameObject.rotate(-2.5,new QVector3D(0,0,1));
+
+this.rotate = function(time)
+{
+    this.GameObject.rotate(-2.5, new QVector3D(0,0,1));
 }
-function jump(){
-  if(this.GameObject.Key_Jump.isActionHeld()){
-    gJumpingBool = true;
-  }
-  if(gJumpingBool == true){
-    if(gCurrentlyFrameJumpInt <= gMaxFrameJumpInt){
-      this.GameObject.setPosition(this.GameObject.position.x(),this.GameObject.position.y()+0.7);
+
+this.jump = function()
+{
+    if (this.GameObject.Key_Jump.isActionHeld()) {
+        gJumpingBool = true;
     }
-    if(gCurrentlyFrameJumpInt > gMaxFrameJumpInt){
-      this.GameObject.setPosition(this.GameObject.position.x(),this.GameObject.position.y()-0.7);
+
+    if (gJumpingBool) {
+        if (gCurrentlyFrameJumpInt <= gMaxFrameJumpInt) {
+            this.GameObject.setPosition(this.GameObject.position.x(), this.GameObject.position.y()+0.7);
+        }
+
+        if (gCurrentlyFrameJumpInt > gMaxFrameJumpInt) {
+            this.GameObject.setPosition(this.GameObject.position.x(), this.GameObject.position.y()-0.7);
+        }
+
+        if (gCurrentlyFrameJumpInt >= gMaxFrameJumpInt*2) {
+            gJumpingBool = false;
+            gCurrentlyFrameJumpInt = 0;
+        }
+
+        ++gCurrentlyFrameJumpInt;
     }
-    if(gCurrentlyFrameJumpInt >= gMaxFrameJumpInt*2){
-      gJumpingBool = false;
-      gCurrentlyFrameJumpInt=0
-    }
-   gCurrentlyFrameJumpInt++;
-  }
 }
-function partOfFrame(time){
- return  time/1000*25;
+
+this.partOfFrame = functiontime)
+{
+    return  time/1000*25;
 }
--- player/examples/ball.gluon/assets/scripts/trap.js
+++ player/examples/ball.gluon/assets/scripts/trap.js
@@ -1,16 +1,23 @@
-function initialize() {
+this.initialize = function()
+{
 }
 
-function update(time){
-  this.GameObject.setPosition(this.GameObject.position.x()-0.5,this.GameObject.position.y());
-  if(this.GameObject.Collider.isColliding()){
-    this.GameObject.destroy();
-    Game.end = true;
-  }
-  if(this.GameObject.position.x() < -50){
-   Game.score += 1;
-   this.GameObject.destroy(); 
-  }
+this.update = function(time)
+{
+    this.GameObject.setPosition(this.GameObject.position.x()-0.5, this.GameObject.position.y());
+    if (this.GameObject.Collider.isColliding())
+    {`
+        this.GameObject.destroy();
+        Game.end = true;
+    }
+
+    if (this.GameObject.position.x() < -50)
+    {
+        ++Game.score;
+        this.GameObject.destroy();
+    }
 }
-function draw(){
+
+this.draw = function()
+{
 }
