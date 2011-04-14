223351b50e2941891c23c0417acaa7c61f1f1909
Laszlo Papp <djszapi@archlinux.us>
2011-04-14 08:16:00 +0200

    Fix the scripts up according to the new api and also format changes

--- player/examples/ball.gluon/assets/scripts/controller.js
+++ player/examples/ball.gluon/assets/scripts/controller.js
@@ -5,13 +5,17 @@ var gLastScoreUpdateInt = 0;
 var gEndedGameBool = false;
 var gTrapReference = null;
 var gStarReference = null;
-function initialize(){
-    Component.paused = false;
+
+this.initialize = function()
+{
+    this.Component.paused = false;
 }
-function start(){
+
+this.start = function()
+{
   Game.score = 0;
   Game.end = false;
-  
+
   gTrapReference = Game.getFromScene("Trap");
   gStarReference = Game.getFromScene("Star");
   gPlayerReference = Game.clone(Game.getFromScene("Player"));
@@ -19,52 +23,59 @@ function start(){
   gPlayerReference.enabled = true;
 }
 
-function update(time){
-  if(Game.end == false){
+this.update = function(time)
+{
+  if (Game.end == false){
     this.deployTrap();
-    if(Game.score%6 ==0 && Game.score != gLastScoreUpdateInt){
+    if (Game.score%6 ==0 && Game.score != gLastScoreUpdateInt){
       this.updateScore(Game.score/6);
     }
-  }else{
-    if(gEndedGameBool==false){
+  } else {
+    if (gEndedGameBool == false) {
       this.end();
     }
-    if(this.GameObject.Key_Reset.isActionHeld()){
+
+    if (this.GameObject.Key_Reset.isActionHeld()) {
       this.resetGame();
     }
   }
 
-    
+
 }
-function deployTrap(){
-  gCurrentlyBreakInt++;
-  if(gCurrentlyBreakInt >=gMiniumBreakInt){
-    if(Math.random()*11 > 9){
+
+this.deployTrap = function()
+{
+  ++gCurrentlyBreakInt;
+  if (gCurrentlyBreakInt >= gMiniumBreakInt) {
+    if (Math.random()*11 > 9){
       var trap = Game.clone(gTrapReference);
-      trap.setPosition(50,-40);
+      trap.setPosition(50, -40);
       trap.enabled = true;
       gCurrentlyBreakInt =0;
     }
   }
-
 }
-function updateScore(score){
-  gLastScoreUpdateInt =score*6;
+
+this.updateScore = function(score) {
+  gLastScoreUpdateInt = score * 6;
   var star =Game.clone(gStarReference);
-  star.setPosition(((score%6)*8)-20,Math.floor(score/6)*8);
+  star.setPosition(((score%6)*8)-20, Math.floor(score/6)*8);
   star.enabled=true;
 
 }
-function end(){
+
+this.end = function()
+{
   gEndedGameBool = true;
   var score =Game.clone(Game.getFromScene("ScoreText"));
-  score.setPosition(-30,0);
+  score.setPosition(-30, 0);
   score.enabled = true;
-  var resetText =Game.clone(Game.getFromScene("ResetText"));
-  resetText.setPosition(-5,-10);
+  var resetText = Game.clone(Game.getFromScene("ResetText"));
+  resetText.setPosition(-5, -10);
   resetText.enabled = true;
-  
 }
-function resetGame(){
+
+this.resetGame = function()
+{
   Game.resetCurrentScene();
 }
