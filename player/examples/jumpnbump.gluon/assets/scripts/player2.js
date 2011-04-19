62ad4a03a34f220bf75519be92d177e9d1d6b4d4
Laszlo Papp <djszapi@archlinux.us>
2011-04-20 00:01:44 +0200

    Player/Examples/JumpnBump: Add the Player2 and relevant stuff to the game

---
+++ player/examples/jumpnbump.gluon/assets/scripts/player2.js
@@ -0,0 +1,47 @@
+var gMaxFrameJumpInt = 25;
+var gCurrentlyFrameJumpInt=0
+var gJumpingBool = false;
+
+this.initialize = function()
+{
+}
+
+this.update = function(time)
+{
+    this.jump();
+}
+
+this.draw = function()
+{
+}
+
+this.rotate = function()
+{
+    this.GameObject.rotate(-2.5, new QVector3D(0,0,1));
+}
+
+this.jump = function()
+{
+    if (this.GameObject.Key_Jump.isActionHeld()) {
+        gJumpingBool = true;
+    }
+
+    if (gJumpingBool) {
+        if (gCurrentlyFrameJumpInt <= gMaxFrameJumpInt) {
+            this.GameObject.setPosition(this.GameObject.position.x(), this.GameObject.position.y()+0.7);
+        } else {
+            this.GameObject.setPosition(this.GameObject.position.x(), this.GameObject.position.y()-0.7);
+            if (gCurrentlyFrameJumpInt >= gMaxFrameJumpInt*2) {
+                gJumpingBool = false;
+                gCurrentlyFrameJumpInt = 0;
+            }
+        }
+
+        ++gCurrentlyFrameJumpInt;
+    }
+}
+
+this.partOfFrame = function(time)
+{
+    return  time/1000*25;
+}
