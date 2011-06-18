603694a36749e046bbe3764419b16eba8349b4a1
Laszlo Papp <lpapp@kde.org>
2011-06-18 22:01:41 +0300

    Add simple player scripts for up/down movement and a menu scene

---
+++ player/examples/pong.gluon/assets/scripts/player2.js
@@ -0,0 +1,52 @@
+this.speed = 30;
+
+this.initialize = function()
+{
+}
+
+this.move = function(time)
+{
+    var addY = this.speed * (time/1000);
+
+    if(this.GameObject.Key_Up.isActionHeld())
+    {
+        this.GameObject.translate(0, addY, 0);
+    } 
+    else if(this.GameObject.Key_Down.isActionHeld())
+    {
+        this.GameObject.translate(0, -addY, 0);
+    }
+
+    var pos = this.GameObject.position;
+    if(pos.x() < -30)
+    {
+        this.GameObject.setPosition(-30, pos.y(), pos.z());
+    }
+    if(pos.x() > 30)
+    {
+        this.GameObject.setPosition(30, pos.y(), pos.z());
+    }
+
+    if(pos.y() < -30)
+    {
+        this.GameObject.translate(0, 5 * (time/1000), 0);
+    }
+    else
+    {
+        this.justSpawned = false;
+    }
+}
+
+this.update = function(time)
+{
+    if(this.Scene.paused)
+        return;
+
+    this.move(time);
+
+}
+
+this.draw = function()
+{
+}
+
