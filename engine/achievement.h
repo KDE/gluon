84e92567d213f73fbc41439406fba801ed04c924
Felix Rohrbach <fxrh@gmx.de>
2011-06-23 17:44:42 +0200

    Add the Achievement class.
    This is the first version of an Achievement class. It can be connected
    with an AbstractStatistic object. Achievements are managed by
    AchievementsAsset objects.
    
    This commit also makes the value property in the AbstractStatistic
    class public, as it is needed by the Achievement class.
    
    REVIEW: 101900

---
+++ engine/achievement.h
@@ -0,0 +1,72 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2011 Felix Rohrbach <fxrh@gmx.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef GLUON_ENGINE_ACHIEVEMENT_H
+#define GLUON_ENGINE_ACHIEVEMENT_H
+
+#include "gluon_engine_export.h"
+
+#include <core/gluonobject.h>
+#include <core/referencecounter.h>
+
+namespace GluonEngine
+{
+    class AbstractStatistic;
+
+    /**
+     * The Achievement class is connected to one statistic. If the statistic reaches a
+     * certain score, achieved() returns true.
+     */
+    class GLUON_ENGINE_EXPORT Achievement : public GluonCore::GluonObject, public GluonCore::ReferenceCounter
+    {
+            Q_OBJECT
+            GLUON_OBJECT( GluonEngine::Achievement )
+
+            /**
+             * The statistic the achievement is connected to
+             */
+            Q_PROPERTY( GluonEngine::AbstractStatistic* statistic READ statistic WRITE setStatistic )
+
+            /**
+             * The score you need to get the achievement
+             */
+            Q_PROPERTY( qlonglong minimumScore READ minimumScore WRITE setMinimumScore )
+
+        public:
+            Q_INVOKABLE Achievement( QObject* parent = 0 );
+            virtual ~Achievement();
+
+            AbstractStatistic* statistic() const;
+            void setStatistic( AbstractStatistic* statistic );
+
+            qlonglong minimumScore() const;
+            void setMinimumScore( qlonglong score );
+
+            /** Has the statistic achieved the goal? */
+            bool achieved() const;
+
+        private:
+            class AchievementPrivate;
+            AchievementPrivate* d;
+    };
+}
+
+Q_DECLARE_METATYPE( GluonEngine::Achievement* )
+
+#endif //GLUON_ENGINE_ACHIEVEMENT_H
