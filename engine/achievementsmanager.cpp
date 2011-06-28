3752ac58758eb9ffc2991c94c72fb30434eb1316
Felix Rohrbach <fxrh@gmx.de>
2011-06-28 19:29:36 +0200

    Add the AchievementsManager class.
    This class saves the achievements information into a separate file, so
    that achievements can be shown without opening the whole project.

---
+++ engine/achievementsmanager.cpp
@@ -0,0 +1,142 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2011 Felix Rohrbach <fxrh@gmx.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "achievementsmanager.h"
+
+#include <engine/achievement.h>
+#include <core/gluonobject.h>
+#include <core/directoryprovider.h>
+#include <core/gdlhandler.h>
+
+#include <QtCore/QDir>
+#include <QtCore/QTextStream>
+#include <QtCore/QDebug>
+
+using namespace GluonEngine;
+
+AchievementsManager::AchievementsManager( QObject* parent )
+    : QObject( parent )
+{
+}
+
+AchievementsManager::~AchievementsManager()
+{
+}
+
+void AchievementsManager::load( const QString& directory )
+{
+    //cleanup
+    foreach( QObject* child, children() )
+    {
+        child->setParent(0);
+        delete child;
+    }
+
+    QDir::home().mkpath(directory);
+    QList<GluonCore::GluonObject*> objectList = GluonCore::GDLHandler::instance()->parseGDL( directory + "/achievementsCache.gdl" );
+    if( objectList.isEmpty() )
+        return;
+
+    GluonCore::GluonObject* tmpObject = objectList[0];
+    foreach( QObject* object, tmpObject->children() )
+    {
+        GluonCore::GluonObject* gluonObject = qobject_cast<GluonCore::GluonObject*>(object);
+        if( gluonObject )
+            gluonObject->setParent(this);
+    }
+    delete tmpObject;
+}
+
+void AchievementsManager::readFromProject( const QList< Achievement* >& achievements )
+{
+    qDebug() << "Achievement size: " << achievements.count();
+    //cleanup
+    foreach( QObject* child, children() )
+    {
+        child->setParent(0);
+        delete child;
+    }
+
+    foreach( Achievement* achievement, achievements )
+    {
+        GluonCore::GluonObject* object = new GluonCore::GluonObject(this);
+        object->setProperty( "path", achievement->fullyQualifiedName() );
+        object->setProperty( "name", achievement->name() );
+        object->setProperty( "achieved", achievement->achieved() );
+    }
+}
+
+void AchievementsManager::makeTemplate()
+{
+    foreach( QObject* child, children() )
+    {
+        GluonCore::GluonObject* object = qobject_cast<GluonCore::GluonObject*>(child);
+        if( object )
+            object->setProperty( "achieved", false );
+    }
+}
+
+void AchievementsManager::save( const QString& directory )
+{
+    GluonCore::GluonObject tmpObject;
+    foreach( QObject* child, children() )
+    {
+        GluonCore::GluonObject* object = qobject_cast<GluonCore::GluonObject*>(child);
+        if( object )
+            object->setParent(&tmpObject);
+    }
+    QString gdlData = GluonCore::GDLHandler::instance()->toGDL(&tmpObject);
+    QDir::home().mkpath(directory);
+    QFile file( directory + "/achievementsCache.gdl" );
+    if( !file.open(QIODevice::WriteOnly) )
+        return;
+
+    QTextStream stream( &file );
+    stream << gdlData;
+    file.close();
+
+    foreach( QObject* object, tmpObject.children() )
+    {
+        GluonCore::GluonObject* gluonObject = qobject_cast<GluonCore::GluonObject*>(object);
+        if( gluonObject )
+            gluonObject->setParent(this);
+    }
+}
+
+int AchievementsManager::achievementsCount() const
+{
+    return children().count();
+}
+
+QString AchievementsManager::achievementPath(int index) const
+{
+    return children()[index]->property("path").toString();
+}
+
+QString AchievementsManager::achievementName(int index) const
+{
+    return children()[index]->property("name").toString();
+}
+
+bool AchievementsManager::achievementAchieved(int index) const
+{
+    return children()[index]->property("achieved").toBool();
+}
+
+#include "achievementsmanager.moc"
---
+++ engine/achievementsmanager.h
@@ -0,0 +1,77 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2011 Felix Rohrbach <fxrh@gmx.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef GLUON_ENGINE_ACHIEVEMENTSMANAGER_H
+#define GLUON_ENGINE_ACHIEVEMENTSMANAGER_H
+
+#include <engine/gluon_engine_export.h>
+
+#include <engine/savable.h>
+
+#include <QtCore/QObject>
+
+namespace GluonEngine
+{
+    class Achievement;
+
+    /**
+     * The AchievementsManager class creates and reads a file, which includes
+     * all Achievements of a game and if the user has achieved these. It can
+     * be used to display the achievements of a user without creating the
+     * whole project.
+     * Additionally, a template file without user information can be saved, so
+     * that you can show the existing achievements even if the user never played
+     * the game.
+     * In the future, this class might be used to sync the local achievements with
+     * achievements saved on a server.
+     */
+    class GLUON_ENGINE_EXPORT AchievementsManager : public QObject
+    {
+            Q_OBJECT
+        public:
+            AchievementsManager( QObject* parent = 0 );
+            virtual ~AchievementsManager();
+
+            /** Load the achievements from this directory */
+            void load( const QString& directory );
+
+            /** Read the achievements information from the achievement list */
+            void readFromProject( const QList<Achievement*>& achievements );
+
+            /** Delete all user specific information from the achievement */
+            void makeTemplate();
+
+            /** Save the achievements information into this directory */
+            void save( const QString& directory );
+
+            /** Number of achievements saved in this class */
+            int achievementsCount() const;
+
+            /** Get the path of the achievement at index */
+            QString achievementPath( int index ) const;
+
+            /** Get the name of the achievement at index */
+            QString achievementName( int index ) const;
+
+            /** Has the user the achievement at index? */
+            bool achievementAchieved( int index ) const;
+    };
+}
+
+#endif // GLUON_ENGINE_ACHIEVEMENTSMANAGER_H
