a11dd37747b837336546c6dc917a8d8df729c49e
Laszlo Papp <lpapp@kde.org>
2011-06-29 05:36:37 +0300

    Use the own installed sound files instead of the "builtin" alsa files

--- audio/examples/play3d/main.cpp
+++ audio/examples/play3d/main.cpp
@@ -26,9 +26,9 @@
 
 int main( int argc, char* argv[] )
 {
-    GluonAudio::Sound* left = new GluonAudio::Sound( "/usr/share/sounds/alsa/Front_Left.wav" );
-    GluonAudio::Sound* right = new GluonAudio::Sound( "/usr/share/sounds/alsa/Front_Right.wav" );
-    GluonAudio::Sound* center = new GluonAudio::Sound( "/usr/share/sounds/alsa/Front_Center.wav" );
+    GluonAudio::Sound* left = new GluonAudio::Sound( "/usr/share/gluon/audio/sounds/Front_Left.wav" );
+    GluonAudio::Sound* right = new GluonAudio::Sound( "/usr/share/gluon/audio/sounds/Front_Right.wav" );
+    GluonAudio::Sound* center = new GluonAudio::Sound( "/usr/share/gluon/audio/sounds/Front_Center.wav" );
 
     left->setPosition( -1, 0, 0 );
     right->setPosition( 1, 0, 0 );
