b955a25a5660a4b6808dc68db8a59428e96c530a
Laszlo Papp <lpapp@kde.org>
2011-07-06 21:04:03 +0300

    Remove the libvorbis dependency from the cpack deb/rpm lines

--- core/singleton.h
+++ core/singleton.h
@@ -1,6 +1,7 @@
 /******************************************************************************
  * This file is part of the Gluon Development Platform
- * Copyright (c) 2009 Arjen Hiemstra <ahiemstra@heimr.nl>
+ * Copyright (c) 2009-2011 Arjen Hiemstra <ahiemstra@heimr.nl>
+ * Copyright (c) 2011 Laszlo Papp <lpapp@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
