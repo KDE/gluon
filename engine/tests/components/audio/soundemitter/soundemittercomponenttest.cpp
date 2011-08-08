d0baef517e17be7ca05211b6c85f3f49c973a407
Laszlo Papp <lpapp@kde.org>
2011-08-08 21:06:33 +0300

    Add soundemittercomponent, soundlistener test classes with few trivial cases.
    It is not included into the build yet because it needs some consideration how to
    actually manage the test classes from subfolder.

---
+++ engine/tests/components/audio/soundemitter/soundemittercomponenttest.cpp
@@ -0,0 +1,109 @@
+/*
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2011 Laszlo Papp <lpapp@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "soundemittercomponenttest.h"
+
+#include <engine/gameobject.h>
+#include <engine/components/sound/soundemitter/soundemittercomponent.h>
+
+#include <QtTest/QtTest>
+
+using namespace GluonEngine;
+
+SoundEmitterComponentTest::SoundEmitterComponentTest()
+{
+}
+
+SoundEmitterComponentTest::~SoundEmitterComponentTest()
+{
+}
+
+void SoundEmitterComponentTest::testCategory()
+{
+    Component component;
+    QVERIFY(component.category() == "Audio");
+}
+
+void SoundEmitterComponentTest::testAsset()
+{
+}
+
+void SoundEmitterComponentTest::testInitialize()
+{
+}
+
+void SoundEmitterComponentTest::testStart()
+{
+}
+
+void SoundEmitterComponentTest::testDraw()
+{
+}
+
+void SoundEmitterComponentTest::testStop()
+{
+}
+
+void SoundEmitterComponentTest::testCleanup()
+{
+}
+
+void SoundEmitterComponentTest::testRadius()
+{
+    SoundEmitterComponent soundEmitterComponent;
+    float radius = 1.0;
+    soundEmitterComponent.setRadius(radius);
+    QCOMPARE(soundEmitterComponent.radius(), radius);
+}
+
+void SoundEmitterComponentTest::testVolume()
+{
+    SoundEmitterComponent soundEmitterComponent;
+    float volume = 1.0;
+    soundEmitterComponent.setVolume(volume);
+    QCOMPARE(soundEmitterComponent.volume(), volume);
+}
+
+void SoundEmitterComponentTest::testPitch()
+{
+    SoundEmitterComponent soundEmitterComponent;
+    float pitch = 1.0;
+    soundEmitterComponent.setPitch(pitch);
+    QCOMPARE(soundEmitterComponent.pitch(), pitch);
+}
+
+void SoundEmitterComponentTest::testIsPlaying()
+{
+}
+
+void SoundEmitterComponentTest::testIsLooping()
+{
+    SoundEmitterComponent soundEmitterComponent;
+    bool isLooping = true;
+    soundEmitterComponent.setLoop(isLooping);
+    QCOMPARE(soundEmitterComponent.isLooping(), isLooping);
+}
+
+void SoundEmitterComponentTest::testPopulateMetaInfo()
+{
+}
+
+QTEST_MAIN(SoundEmitterSoundEmitterComponentTest)
+
+#include "soundemittercomponenttest.moc"
