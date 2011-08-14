8e65aeb192356abc98eb94fb337b6804cd5253f9
Laszlo Papp <lpapp@kde.org>
2011-08-14 12:10:37 +0300

    Partially implement the Redo/Undo operation for material instances

--- creator/lib/objectmanager.h
+++ creator/lib/objectmanager.h
@@ -30,6 +30,11 @@ namespace GluonCore
     class GluonObject;
 }
 
+namespace GluonGraphics
+{
+    class MaterialInstance;
+}
+
 namespace GluonEngine
 {
     class Asset;
@@ -52,6 +57,7 @@ namespace GluonCreator
             GluonEngine::GameObject* createNewGameObject();
             GluonEngine::Asset* createNewAsset( const QString& fileName, GluonCore::GluonObject* parent = 0, const QString& className = QString(), const QString& name = QString() );
             void createAssets( const QStringList& fileNames, GluonCore::GluonObject* = 0 );
+            void createObjectCommand(GluonGraphics::MaterialInstance* materialInstance);
 
             void deleteGameObject( GluonEngine::GameObject* object );
             void changeProperty( GluonCore::GluonObject* object, QString& property, QVariant& oldValue, QVariant& newValue );
