8c3fc3cfecfbeb37e1711e839aa1a98809d3f7ac
Pranav Ravichandran <prp.1111@gmail.com>
2011-08-26 17:17:08 +0530

    Moved btglobal.h to lib

---
+++ smarts/lib/btglobal.h
@@ -0,0 +1,19 @@
+#ifndef BTGLOBAL_H
+#define BTGLOBAL_H
+
+#include <QVariant>
+#include "btlib_export.h"
+
+struct BT_LIB_EXPORT btChildWeights 
+{
+    QVariantList childWeightList;
+};
+
+struct BT_LIB_EXPORT btParallelConditions
+{
+    QVariantList parallelConditions;
+};
+
+Q_DECLARE_METATYPE(btChildWeights)
+Q_DECLARE_METATYPE(btParallelConditions)
+#endif
