aa02d6e437eeb48e01925d64086533d4c88f3784
Felix Rohrbach <fxrh@gmx.de>
2011-09-07 18:54:27 +0200

    Engine: Buttons for PrefabInstance / PrefabInstanceChild
    Add a revert and a apply button to the properties view if the object
    inherits AbstractPrefabInstance.

--- creator/lib/widgets/prefabcontrols.cpp
+++ creator/lib/widgets/prefabcontrols.cpp
@@ -1,6 +1,7 @@
 /******************************************************************************
  * This file is part of the Gluon Development Platform
  * Copyright (C) 2011 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ * Copyright (C) 2011 Felix Rohrbach <fxrh@gmx.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,13 +20,54 @@
 
 #include "prefabcontrols.h"
 
+#include <engine/abstractprefabinstance.h>
+
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QPushButton>
+
 using namespace GluonCreator;
 
-PrefabControls::PrefabControls(QWidget* parent, Qt::WindowFlags f)
+PrefabControls::PrefabControls( GluonEngine::AbstractPrefabInstance* instance, QWidget* parent, Qt::WindowFlags f )
     : QWidget(parent, f)
+    , m_instance( instance )
 {
+    m_layout = new QHBoxLayout();
+    m_revertButton = new QPushButton( tr("Revert") );
+    if( !instance->hasGameObjectChanged() )
+        m_revertButton->setEnabled( false );
+
+    m_applyButton = new QPushButton( tr("Apply") );
+    if( !instance->hasGameObjectChanged() )
+        m_applyButton->setEnabled( false );
+
+    m_layout->addWidget( m_revertButton );
+    m_layout->addWidget( m_applyButton );
+    setLayout( m_layout );
+
+    connect( instance, SIGNAL(instanceChanged()), this, SLOT(enableButtons()) );
+    connect( instance, SIGNAL(instanceReverted()), this, SLOT(disableButtons()) );
+    connect( instance, SIGNAL(instanceStored()), this, SLOT(disableButtons()) );
+    connect( m_revertButton, SIGNAL(clicked()), instance, SLOT(revertChanges()) );
+    connect( m_applyButton, SIGNAL(clicked()), instance, SLOT(storeChanges()) );
 }
 
 PrefabControls::~PrefabControls()
 {
+}
+
+void PrefabControls::propertyChanged()
+{
+    m_instance->gameObjectChanged();
+}
+
+void PrefabControls::enableButtons()
+{
+    m_applyButton->setEnabled( true );
+    m_revertButton->setEnabled( true );
+}
+
+void PrefabControls::disableButtons()
+{
+    m_applyButton->setEnabled( false );
+    m_revertButton->setEnabled( false );
 }
