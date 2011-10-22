038e027092f75ff2f1c8062655c0798b151d06d8
Dan Leinir Turthra Jensen <admin@leinir.dk>
2011-10-22 17:43:11 +0100

    Fix copyright headers

--- smarts/lib/btlib_export.h
+++ smarts/lib/btlib_export.h
@@ -1,22 +1,23 @@
-/*  This file is part of the Gluon library.
-    Copyright 2009 Morten Justesen <morten.justesen@gmail.com>
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
-*/
-
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2009 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ * Copyright (c) 2009 Kim Jung Nissen <jungnissen@gmail.com>
+ * Copyright (c) 2009 Morten Justesen <morten.justesen@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #ifndef BT_LIB_EXPORT_H
 #define BT_LIB_EXPORT_H
