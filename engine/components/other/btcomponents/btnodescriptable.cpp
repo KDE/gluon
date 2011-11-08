f9537d8718ef82789b7de2c36b72eec5a53bc1a6
Dan Leinir Turthra Jensen <admin@leinir.dk>
2011-11-08 19:35:23 +0000

    Fix up a whole bunch of copyright header issues in the SMARTS components

--- engine/components/other/btcomponents/btnodescriptable.cpp
+++ engine/components/other/btcomponents/btnodescriptable.cpp
@@ -1,13 +1,34 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (C) 2010 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include "btnodescriptable.h"
 
 #include <QtScript/QScriptEngine>
 #include <QtCore/QMimeData>
 #include <engine/gameobject.h>
 #include <QtCore/QDebug>
-#include <engine/game.h>
+
+#include "engine/game.h"
+#include "engine/scene.h"
+
 #include "btcharacterscriptable.h"
 #include "character.h"
-#include <engine/scene.h>
 
 using namespace GluonEngine;
 
