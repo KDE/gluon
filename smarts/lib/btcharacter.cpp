69ce0bef875d650ce962f66e6e130679de81d0bc
Laszlo Papp <ext-laszlo.papp@nokia.com>
2011-11-22 14:16:33 +0200

    Revert "Fix the unfortunate warning in the think() method of the btcharacter class"
    This reverts commit a0b4db49ded634f880549f28dbf0493e8e08c119.

--- smarts/lib/btcharacter.cpp
+++ smarts/lib/btcharacter.cpp
@@ -69,11 +69,6 @@ void btCharacter::setBehaviorTree(btNode* behaviorTree)
 	this->setFile(brain->getFile());
 }
 
-// For avoiding the build time warnings of gcc since the code looks fine
-// See the following report for further details:
-// https://bugreports.qt.nokia.com/browse/QTBUG-5915
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wstrict-overflow"
 void btCharacter::think()
 {
 	//local variables used in think
@@ -297,7 +292,6 @@ void btCharacter::think()
 		m_thinksDone++;
 	}
 }
-#pragma GCC diagnostic pop
 
 void btCharacter::stopParallelExecution(btNode * currentNode, QStack<btNode*>* parentStack)
 {    
