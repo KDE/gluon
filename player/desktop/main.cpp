64e1ce22a02ce2410b2c58039167883547da869f
Claudio Desideri <happy.snizzo@gmail.com>
2013-06-11 16:49:39 +0200

    Added simple base structure. Needs integration into build system.

--- player/desktop/main.cpp
+++ player/desktop/main.cpp
@@ -1,67 +1,23 @@
-/******************************************************************************
- * This file is part of the Gluon Development Platform
- * Copyright (c) 2010 Laszlo Papp <lpapp@kde.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
+#include <KApplication>
+#include <KAboutData>
+#include <KCmdLineArgs>
+#include <KLocale>
+ 
 #include "mainwindow.h"
-
-#include <KDE/KApplication>
-#include <KDE/KAboutData>
-#include <KDE/KCmdLineArgs>
-#include <KDE/KMessageBox>
-
-#include <QtCore/QFile>
-
-int main( int argc, char** argv )
+ 
+int main (int argc, char *argv[])
 {
-    KAboutData aboutData( "gluonplayer-desktop", 0,
-                          ki18n( "Gluon Desktop Player" ), GLUON_VERSION_STRING.toUtf8(),
-                          ki18n( "KDE Desktop Player for Gluon games." ),
-                          KAboutData::License_LGPL_V2,
-                          ki18n( "Copyright 2010 Laszlo Papp" ),
-                          KLocalizedString(),
-                          "???"
-                        );
-
-    aboutData.setProgramIconName( "Desktop Player" );
-    aboutData.addAuthor( ki18n( "Laszlo Papp" ), ki18n( "KDE Desktop Player" ), "lpapp@kde.org" );
-    aboutData.setProductName( "gluon/gluonplayer" );
-
-    KCmdLineArgs::init( argc, argv, &aboutData );
-    KCmdLineOptions options;
-
-    options.add( "+file", ki18n( "File to open" ) );
-    KCmdLineArgs::addCmdLineOptions( options );
-
-    KApplication app;
-
-    if( argc > 1 && !QFile::exists( argv[1] ) )
-    {
-        if( KMessageBox::warningContinueCancel( 0, i18n( "File does not exist: %1! Do you want to continue?", argv[1] ), i18n( "Continue?" ),
-                                                KStandardGuiItem::cont(), KStandardGuiItem::quit() ) == KMessageBox::Cancel )
-            return 1;
-
-    }
-
-    KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
-
-    GluonKDEPlayer::MainWindow* window = new GluonKDEPlayer::MainWindow( args->count() ? args->arg( 0 ) : "" );
-    window->show();
-
-    app.exec();
+  KAboutData aboutData( "gluonplayerdesktop", 0,
+      ki18n("Gluon Player"), "0.1",
+      ki18n("Hello world from the gluon player :)"),
+      KAboutData::License_GPL,
+      ki18n("Copyright (c) 2007 to_be_written") );
+  KCmdLineArgs::init( argc, argv, &aboutData );
+ 
+  KApplication app;
+ 
+  MainWindow* window = new MainWindow();
+  window->show();
+ 
+  return app.exec();
 }
-
