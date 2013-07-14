f7a3e8df3af2a5ba8c96f9f6abbd51a3551d2a94
Claudio Desideri <happy.snizzo@gmail.com>
2013-07-14 18:24:58 +0200

    Changed server to official gamingfreedom.org

--- player/lib/serviceprovider.cpp
+++ player/lib/serviceprovider.cpp
@@ -94,14 +94,14 @@ void ServiceProvider::init()
     }
 
     connect( &d->manager, SIGNAL(defaultProvidersLoaded()), SLOT(providersUpdated()) );
-    d->manager.addProviderFile(QUrl("http://localhost/providers.xml"));
+    d->manager.addProviderFile(QUrl("http://www.gamingfreedom.org/providers.xml"));
 }
 
 void ServiceProvider::providersUpdated()
 {
     if( !d->manager.providers().isEmpty() )
     {
-        d->provider = d->manager.providerByUrl( QUrl( "http://localhost/v1/" ) );
+        d->provider = d->manager.providerByUrl( QUrl( "http://www.gamingfreedom.org/v1/" ) );
 
         if( !d->provider.isValid() )
         {
