a0f05415687f54605dd5706c388591f2bb37dea2
Felix Rohrbach <fxrh@gmx.de>
2015-02-25 00:42:52 +0100

    Graphics: Port Filemesh example
    REVIEW: 122722

--- graphics/examples/filemesh/main.cpp
+++ graphics/examples/filemesh/main.cpp
@@ -17,16 +17,17 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "graphics/renderwidget.h"
+#include "graphics/renderwindow.h"
 
 #include <cmath>
 
-#include <QApplication>
+#include <QtGui/QGuiApplication>
 
 #include "core/directoryprovider.h"
-#include "graphics/manager.h"
+#include "core/resourcemanager.h"
+#include "graphics/defaults.h"
 #include "graphics/camera.h"
-#include "graphics/frustrum.h"
+#include "graphics/frustum.h"
 #include "graphics/filemesh.h"
 #include "graphics/entity.h"
 #include "graphics/world.h"
@@ -34,7 +35,7 @@
 #include "graphics/materialinstance.h"
 #include "graphics/texture.h"
 #include "graphics/rendertarget.h"
-#include <mathutils.h>
+#include "graphics/mathutils.h"
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
@@ -43,34 +44,35 @@ using namespace GluonGraphics;
 
 int main( int argc, char* argv[] )
 {
-    QApplication app( argc, argv );
+    QGuiApplication app( argc, argv );
 
-    RenderWidget widget;
-    widget.show();
+    RenderWindow window;
+    window.show();
 
-    Manager::instance()->initialize();
+    Defaults::initialize();
 
     Eigen::Affine3f mat = Eigen::Affine3f::Identity();
 
     FileMesh* mesh = new FileMesh( GluonCore::DirectoryProvider::instance()->dataDirectory() + "/gluon/examples/graphics/duck.dae" );
-    Manager::instance()->addResource< Mesh >( "duck.dae", mesh );
+    GluonCore::ResourceManager::instance()->addResource< Mesh >( "duck.dae", mesh );
     mesh->initialize();
-    Texture* texture = Manager::instance()->createResource< Texture >( "duck.tga" );
+    Texture* texture = GluonCore::ResourceManager::instance()->createResource< Texture >( "duck.tga" );
     texture->load( GluonCore::DirectoryProvider::instance()->dataDirectory() + "/gluon/examples/graphics/duck.tga" );
 
-    Material* material = Manager::instance()->createResource< Material>( "duck" );
+    Material* material = GluonCore::ResourceManager::instance()->createResource< Material>( "duck" );
     material->load( GluonCore::DirectoryProvider::instance()->dataDirectory() + "/gluon/examples/graphics/duck.gluonmaterial" );
     material->build();
 
+    World* world = GluonCore::ResourceManager::instance()->resource< World >( Defaults::World );
 
-    Entity* ent = Manager::instance()->currentWorld()->createEntity< Entity >();
-    mat.rotate( Eigen::AngleAxis<float>( -M_PI_4 /* pi/4 */, MathUtils::VECTOR_UNIT_Y) );
+    Entity* ent = world->createEntity< Entity >();
+    mat.rotate( Eigen::AngleAxis<float>( -M_PI_4 /* pi/4 */, Eigen::Vector3f(0.f, 1.f, 0.f) ) );
     ent->setTransform( mat );
     ent->setMesh( mesh );
     ent->setMaterialInstance( material->createInstance() );
     ent->materialInstance()->setProperty( "texture0", QVariant::fromValue( texture ) );
 
-    Camera* cam = Manager::instance()->currentWorld()->createEntity< Camera >();
+    Camera* cam = world->createEntity< Camera >();
     mat = Eigen::Affine3f::Identity();
     mat.translate( Eigen::Vector3f(0.f, 75.f, 100.f) );
     cam->setTransform( mat );
@@ -79,7 +81,7 @@ int main( int argc, char* argv[] )
     cam->setNearPlane( 0.f );
     cam->setFarPlane( 1000.f );
 
-    Manager::instance()->resource< RenderTarget >( Manager::Defaults::RenderTarget )->addChild( cam );
+    GluonCore::ResourceManager::instance()->resource< RenderTarget >( Defaults::RenderTarget )->addChild( cam );
 
     //app.exec();
 
